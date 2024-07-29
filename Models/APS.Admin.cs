e353cf957aa2a41fca660e8b933176bf0222dffb
Zhong Wu <wuzhong1980@gmail.com>
2024-07-28 22:25:22 -0400

    use Admin SDK

--- Models/APS.Admin.cs
+++ Models/APS.Admin.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-// using Autodesk.Forge;
-// using Autodesk.Forge.Model;
+using Autodesk.Construction.AccountAdmin;
+using Autodesk.Construction.AccountAdmin.Model;
 using System;
 using RestSharp;
 using Newtonsoft.Json;
@@ -12,90 +12,87 @@ public partial class APS
 
     public async Task<IEnumerable<dynamic>> getProjectsACC(string accountId, Tokens tokens)
     {
-        var client = new RestClient(Base_Domain);
-        var request = new RestRequest("/construction/admin/v1/accounts/"+accountId+"/projects", Method.Get);
-        request.AddHeader("Authorization", "Bearer " + tokens.AccessToken);
-        RestResponse response = await client.ExecuteAsync(request);
-        dynamic projectsData = JsonConvert.DeserializeObject(response.Content);
-        Console.WriteLine(projectsData.results);
-        return projectsData.results;
+        AdminClient adminClient = new AdminClient(_SDKManager);
+        var allProjects = new List<Project>();
+        var offset = 0;
+        var totalResult = 0;
+        do
+        {
+            var projects = await adminClient.GetProjectsAsync(tokens.AccessToken, accountId);
+            allProjects.AddRange(projects.Results);
+            offset +=  (int)projects.Pagination.Limit;
+            totalResult = (int)projects.Pagination.TotalResults;
+        }while (offset < totalResult);
+        return allProjects;
     }
 
     public async Task<IEnumerable<dynamic>> GetProjectACC( string projectId, Tokens tokens)
     {
-        var client = new RestClient(Base_Domain);
-        var request = new RestRequest("/construction/admin/v1/projects/"+projectId, Method.Get);
-        request.AddHeader("Authorization", "Bearer " + tokens.AccessToken);
-        RestResponse response = await client.ExecuteAsync(request);
-        dynamic projectsData = JsonConvert.DeserializeObject(response.Content);
-        Console.WriteLine(projectsData);
-        return new List<dynamic> { projectsData };
+        AdminClient adminClient = new AdminClient(_SDKManager);
+        var project = await adminClient.GetProjectAsync(tokens.AccessToken, projectId );
+        var projects = new List<Project>();
+        projects.Add(project);
+        return projects;
     }
 
 
     public async Task<IEnumerable<dynamic>> GetProjectUsersACC( string projectId, Tokens tokens)
     {
-        var client = new RestClient(Base_Domain);
-        var request = new RestRequest("/construction/admin/v1/projects/" + projectId+"/users", Method.Get);
-        request.AddHeader("Authorization", "Bearer " + tokens.AccessToken);
-        RestResponse response = await client.ExecuteAsync(request);
-        dynamic projectsData = JsonConvert.DeserializeObject(response.Content);
-        Console.WriteLine(projectsData.results);
-        return projectsData.results;
+        AdminClient adminClient = new AdminClient(_SDKManager);
+        var allUsers = new List<ProjectUser>();
+        var offset = 0;
+        var totalResult = 0;
+        do
+        {
+            var users = await adminClient.GetProjectUsersAsync(tokens.AccessToken, projectId);
+            allUsers.AddRange(users.Results);
+            offset += (int)users.Pagination.Limit;
+            totalResult = (int)users.Pagination.TotalResults;
+        } while (offset < totalResult);
+        return allUsers;
     }
 
 
     public async Task<dynamic> CreateProject(string accountId, JObject body, Tokens tokens)
     {
-        var client = new RestClient(Base_Domain);
-        var request = new RestRequest("/construction/admin/v1/accounts/" + accountId + "/projects", Method.Post);
-        request.AddHeader("Authorization", "Bearer " + tokens.AccessToken);
-        request.AddHeader("Content-Type", "application/json");
-        request.AddJsonBody( JsonConvert.SerializeObject( body));
-        RestResponse response = await client.ExecuteAsync(request);
-        dynamic projectsData = JsonConvert.DeserializeObject<JObject>(response.Content);
-        Console.WriteLine(response.StatusCode);
-        return projectsData;
+        AdminClient adminClient = new AdminClient(_SDKManager);
+        ProjectPayload projectPayload = new ProjectPayload();
+        var newProject = await adminClient.CreateProjectAsync(tokens.AccessToken, accountId, projectPayload);
+        return newProject;
     }
 
 
     public async Task<dynamic> AddProjectAdminACC(string projectId, string email, Tokens tokens)
     {
-        var client = new RestClient(Base_Domain);
-        var request = new RestRequest("/construction/admin/v1/projects/" + projectId + "/users", Method.Post);
-        request.AddHeader("Authorization", "Bearer " + tokens.AccessToken);
-        request.AddHeader("Content-Type", "application/json");
-        string body = @"{
-        'email': '"+ email +@"',
-        'products': [{
-            'key': 'projectAdministration',
-            'access': 'administrator'
-        }, {
-            'key': 'docs',
-            'access': 'administrator'
-        }]
-        }";
+        AdminClient adminClient = new AdminClient(_SDKManager);
+        ProjectUserPayload adminUser = new ProjectUserPayload()
+        {
+            Email = email,
+            Products = new List<ProjectUserPayloadProducts>()
+            {
+                new ProjectUserPayloadProducts()
+                {
+                    Key = ProductKeys.ProjectAdministration,
+                    Access = ProductAccess.Administrator
+                },
+                new ProjectUserPayloadProducts()
+                {
+                    Key = ProductKeys.Docs,
+                    Access = ProductAccess.Administrator
 
-        var userBody = JsonConvert.SerializeObject(JObject.Parse(body));
-        request.AddJsonBody(userBody);
-        RestResponse response = await client.ExecuteAsync(request);
-        dynamic projectsData = JsonConvert.DeserializeObject(response.Content);
-        Console.WriteLine(projectsData);
-        return projectsData;
+                }
+            }
+        };
+        var projectUser = await adminClient.AssignProjectUserAsync(tokens.AccessToken, projectId, adminUser);
+        return projectUser;
     }
 
 
     public async Task<dynamic> ImportProjectUsersACC(string projectId, JObject body, Tokens tokens)
     {
-        var options = new RestClientOptions("https://developer.api.autodesk.com");
-        var client = new RestClient(options);
-        var request = new RestRequest(String.Format("/construction/admin/v2/projects/{0}/users:import", projectId), Method.Post);
-        request.AddHeader("Authorization", "Bearer " + tokens.AccessToken);
-        request.AddHeader("Content-Type", "application/json");
-        request.AddJsonBody(JsonConvert.SerializeObject( body));
-        RestResponse response = await client.ExecuteAsync(request);
-        dynamic usersInfo = JsonConvert.DeserializeObject<JObject>(response.Content);
-        Console.WriteLine(usersInfo);
-        return usersInfo;
+        AdminClient adminClient = new AdminClient(_SDKManager);
+        var projectUsersPayload = new ProjectUsersImportPayload();
+        var usersRes = await adminClient.ImportProjectUsersAsync(tokens.AccessToken, projectId, projectUsersPayload);
+        return usersRes;
     }
 }
